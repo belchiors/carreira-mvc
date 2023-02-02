58975d53040074cf9cd10b00e1503bc5ca3b00ef
bis <belchiorisv@gmail.com>
2023-02-02 16:24:28 -0300

    Add User class model

---
+++ src/Carreira/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Carreira.Models;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
