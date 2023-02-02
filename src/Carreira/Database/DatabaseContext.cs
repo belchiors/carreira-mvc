dd625eb01032826af2b426fbee10962dbc9a6e9d
bis <belchiorisv@gmail.com>
2023-02-02 16:38:09 -0300

    Configure database context

--- src/Carreira/Database/DatabaseContext.cs
+++ src/Carreira/Database/DatabaseContext.cs
@@ -1,12 +1,24 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Carreira.Models;
 
 namespace Carreira.Database;
 
 public class DatabaseContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Define email as unique
+        modelBuilder.Entity<User>()
+            .HasIndex(user => user.Email)
+            .IsUnique();
     }
 }
