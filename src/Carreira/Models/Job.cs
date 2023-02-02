4cd970ade150aa00b76da4090172f2cb0512526c
bis <belchiorisv@gmail.com>
2023-02-02 16:28:44 -0300

    Add Job class entity model

---
+++ src/Carreira/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace Carreira.Models;
+
+public class Job
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string CompanyName { get; set; }
+    public string CompanyEmail { get; set; }
+    public string CompanyLocation { get; set; }
+}
