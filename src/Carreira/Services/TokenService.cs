103e274a3cf13d136599dfd9c574633d24a697b2
bis <belchiorisv@gmail.com>
2023-02-02 17:17:11 -0300

    Add class service to generate JTW tokens

---
+++ src/Carreira/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Carreira.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Carreira.Services;
+
+public class TokenService
+{
+    private readonly string _securityKey;
+
+    public TokenService(string securityKey)
+    {
+        _securityKey = securityKey;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityKey = Encoding.ASCII.GetBytes(_securityKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            }),
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(securityKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
