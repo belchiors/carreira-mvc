fcc5810f0237c3fc5653a0fdf2fd88fdeb25a1ca
bis <belchiorisv@gmail.com>
2023-02-02 23:31:54 -0300

    Add footer component

---
+++ src/Carreira/ClientApp/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="page-footer">
+            <div className="wrapper">
+                
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
