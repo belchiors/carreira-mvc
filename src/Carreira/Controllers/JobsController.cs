36dff617c1535e1e47a2a1d57aeaad4a1562437d
bis <belchiorisv@gmail.com>
2023-02-04 15:50:10 -0300

    Create action methods to handle incoming requests

--- src/Carreira/Controllers/JobsController.cs
+++ src/Carreira/Controllers/JobsController.cs
@@ -1,5 +1,8 @@
 using System;
+using Carreira.Database;
+using Carreira.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Carreira.Controllers;
 
@@ -7,14 +10,54 @@ namespace Carreira.Controllers;
 [Route("api/[controller]")]
 public class JobsController : ControllerBase
 {
-    public JobsController()
+    private readonly DatabaseContext _databaseContext;
+
+    public JobsController(DatabaseContext databaseContext)
     {
+        _databaseContext = databaseContext;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Job job)
+    {
+        await _databaseContext.Jobs.AddAsync(job);
+        await _databaseContext.SaveChangesAsync();
+        return Ok(job);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(Job job)
+    {
+        try
+        {
+            _databaseContext.Jobs.Update(job);
+            await _databaseContext.SaveChangesAsync();
+            return Ok(job);
+        }
+        catch(Exception e)
+        {
+            return BadRequest(e.Message);
+        }
 
     }
 
-    [HttpGet]
-    public string Index()
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int jobId)
     {
-        return "";
+        var job = await _databaseContext.Jobs.FindAsync(jobId);
+        if (job == null)
+        {
+            return NotFound("Vaga não encontrada");
+        }
+        _databaseContext.Jobs.Remove(job);
+        await _databaseContext.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var jobs = _databaseContext.Jobs.AsAsyncEnumerable();
+        return Ok(jobs);
     }
 }
