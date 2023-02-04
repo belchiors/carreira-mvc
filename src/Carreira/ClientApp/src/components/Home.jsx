a3d51a531a971b219394ba5ffb393bc0f1fc38e8
bis <belchiorisv@gmail.com>
2023-02-04 18:35:22 -0300

    Add a function to fetch and populate the the list view

--- src/Carreira/ClientApp/src/components/Home.jsx
+++ src/Carreira/ClientApp/src/components/Home.jsx
@@ -1,7 +1,15 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import ListView from "./ListView";
 
 function Home() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("api/jobs")
+            .then((response) => response.json())
+            .then(setItems);
+    }, []);
+
     return (
         <div className="content">
             <ListView items={items} />
