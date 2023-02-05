702ed25da3cc8bbd1fdd15c9413807067b00b394
bis <belchiorisv@gmail.com>
2023-02-05 17:01:55 -0300

    Limit client interactions based on user role

--- src/Carreira/ClientApp/src/App.jsx
+++ src/Carreira/ClientApp/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from './components/Home';
 import Layout from './components/Layout';
@@ -7,20 +7,37 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import JobForm from './components/JobForm';
 
+import PermissionProvider from "./utils/PermissionProvider";
+import { isAuthenticated, getCurrentUser } from "./services/auth";
+
 import './custom.css';
 
+function PrivateRoute({ children }) {
+    if (!isAuthenticated()) {
+        return <Navigate to="/account/signin" replace />;
+    }
+    return children;
+}
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route default path="" element={<Home />} />
-        <Route path="publish" element={<JobForm />} />
-      </Route>
-      <Route path="/account">
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-      </Route>
-    </Routes>
+    const user = getCurrentUser();
+    return (
+        <PermissionProvider role={user?.role}>
+            <Routes>
+                <Route path="/" element={<Layout />} >
+                    <Route default path="" element={<Home />} />
+                    <Route path="publish" element={
+                        <PrivateRoute>
+                            <JobForm />
+                        </PrivateRoute>
+                    } />
+                </Route>
+                <Route path="/account">
+                    <Route path="signin" element={<SignIn />} />
+                    <Route path="signup" element={<SignUp />} />
+                </Route>
+            </Routes>
+        </PermissionProvider>
   );
 }
 
--- src/Carreira/ClientApp/src/components/NavMenu.jsx
+++ src/Carreira/ClientApp/src/components/NavMenu.jsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from "react";
+import Restricted from "../utils/Restricted";
+import { getCurrentUser, isAuthenticated, logout } from "../services/auth";
 
 function NavMenu() {
+    const [user, setUser] = useState(null);
+
+    const onLogout = () => {
+        logout();
+        window.location.reload(true);
+    }
+
+    useEffect(() => {
+        const currentUser = getCurrentUser();
+        setUser(currentUser);
+    }, []);
+
     return (
         <nav className="navbar">
             <a className="brand" href="/">Carreira</a>
             <ul className="nav-menu">
-                <li className="menu-item">
-                    <a href="/publish">Anunciar Vaga</a>
-                </li>
-                <li className="menu-item">
-                    <a href="/account/signin">Login</a>
-                </li>
+                {isAuthenticated() ? (
+                    <>
+                        <Restricted to={"0"}>
+                            <li className="menu-item">
+                                <a href="/publish">Anunciar Vaga</a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="/jobs?userId=1">Meus Anúncios</a>
+                            </li>
+                        </Restricted>
+                        <span className="menu-item">{user?.name}</span>
+                        <li className="menu-item">
+                            <a href="#" onClick={onLogout}>Sair</a>
+                        </li>
+                    </>
+                ) : (
+                        <li className="menu-item">
+                            <a href="/account/signin">Login</a>
+                        </li>
+                    )
+               }
             </ul>
         </nav>
     );
